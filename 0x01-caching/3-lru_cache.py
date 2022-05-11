a54cd0a93a193c71ca769e6b1e642e74581dcb2f
Marcelo <3338@holbertonschool.com>
2022-05-11 11:07:45 -0300

    F

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -3,8 +3,6 @@
 Create a class LRUCache that inherits from
 BaseCaching and is a caching system:
 """
-
-
 BaseCaching = __import__('base_caching').BaseCaching
 
 
@@ -13,43 +11,36 @@ class LRUCache(BaseCaching):
     def __init__(self):
         super().__init__()
         self.list_aux = []
-        self.dict_aux = {}
 
     def put(self, key, item):
         """ Add LRU """
         if key is None or item is None:
             return
-        if key not in self.dict_aux:
-            self.dict_aux[key] = 0
-        else:
-            self.dict_aux[key] += 1
-        for i in self.list_aux:
-            if i == key:
-                self.list_aux.remove(i)
-        self.list_aux.append(key)
         self.cache_data[key] = item
-        if len(self.cache_data) > self.MAX_ITEMS:
-            min_value = min(self.dict_aux.values())
-            min_keys = []
-            for key in self.dict_aux:
-                if self.dict_aux[key] == min_value:
-                    min_keys.append(key)
-
+        if self.list_aux.count(key) == 0:
+            self.list_aux.append(key)
+        else:
             for i in self.list_aux:
-                if i in min_keys:
-                    to_remove = i
+                if i == key:
                     self.list_aux.remove(i)
                     break
-            del self.cache_data[to_remove]
+            self.list_aux.append(key)
+        if len(self.cache_data) > self.MAX_ITEMS:
+            del self.cache_data[self.list_aux[0]]
+            to_remove = self.list_aux[0]
+            self.list_aux.remove(self.list_aux[0])
             print("DISCARD: {}".format(to_remove))
 
     def get(self, key):
         """ Get LRU """
         if key is None or key not in self.cache_data:
             return None
-        self.dict_aux[key] += 1
-        for i in self.list_aux:
-            if i == key:
-                self.list_aux.remove(i)
-        self.list_aux.append(key)
+        if self.list_aux.count(key) == 0:
+            self.list_aux.append(key)
+        else:
+            for i in self.list_aux:
+                if i == key:
+                    self.list_aux.remove(i)
+                    break
+            self.list_aux.append(key)
         return self.cache_data[key]
