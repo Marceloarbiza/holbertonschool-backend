1f20f9557c5c19c8b2607a6d836e46e2e3721201
Marcelo <3338@holbertonschool.com>
2022-05-17 22:05:34 -0300

    D

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -28,6 +28,8 @@ app.config.from_object(Config)
 @babel.localeselector
 def get_locale():
     """ Returns current locale """
+    if 'locale' in request.args:
+        return request.args['locale']
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
